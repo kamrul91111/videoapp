4432dfd55dc0943e974b9d1157afb00c46289a47
Kamrul Islam <kamrul.islam91111@gmail.com>
2021-11-29 21:59:47 +1100

    can render videos and playlists, could not finish rest of the features

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,7 +20,6 @@ const Home = () => {
     axios
       .get(`${mongoServer}/videos`)
       .then(res => {
-        console.log(res.data);
         setLoading(false); // stop loading
         setData(res.data); // grab data from server
       })
@@ -34,28 +33,31 @@ const Home = () => {
   }, []); // no dependencies, shoot function upon rendering component
 
   return (
-    <div className='video-container'>
-      {/* loop through array and grab items */}
-      {data.map(item => (
-        <Card sx={{maxWidth: 345, height: 400}}  style={{margin: 15}}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="200"
-              image={item.thumbnail}
-              alt={item.name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {item.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Posted on: {Date(item.dateCreated).substring(0, 15)}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
+    <div style={{padding: 40}}>
+      <h3 style={{textAlign: "center"}}>Videos</h3>
+      <div className="video-container">
+        {/* loop through array and grab items */}
+        {data.map(item => (
+          <Card key={item.id} sx={{maxWidth: 345, height: 400}} style={{margin: 15}}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="200"
+                image={item.thumbnail}
+                alt={item.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Posted on: {Date(item.dateCreated).substring(0, 15)}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Playlists/Playlists.jsx
+++ src/pages/Playlists/Playlists.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import "./playlists.css";
+
+import axios from "axios";
+
+// api
+import {mongoServer} from "../../api/apiUrl";
 
 const Playlists = () => {
-    return (
-        <div>
-            boo
-        </div>
-    )
-}
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); // start loading
 
-export default Playlists
+  useEffect(() => {
+    axios.get(`${mongoServer}/playlists`).then(res => {
+      console.log(res.data);
+      setData(res.data); // grab data
+      setLoading(false); // stop loading
+    });
+
+    // cleanup
+    return () => {
+      setData([]);
+      setLoading(true);
+    };
+  }, []);
+  return (
+    <div className="playlist-container">
+      <h3 style={{textAlign: "center"}}>Your Playlist</h3>
+      {/* map through data and render UI */}
+      {data.map(item => (
+        <div key={item.id}>
+          <h4>{item.name}</h4>
+        </div>
+      ))}
+      <h4></h4>
+    </div>
+  );
+};
+
+export default Playlists;
